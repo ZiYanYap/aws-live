d3048201833e4fb0c55cd16a91be9104ba3abd78
Yap Zi Yan <ziyanyapzyy@gmail.com>
2024-09-06 11:18:50 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cn3wnncr4unn.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "yapziyan-employee"
+customregion = "us-east-1"
+
